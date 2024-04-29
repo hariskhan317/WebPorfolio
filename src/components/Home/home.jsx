ed107688f020cc5a12059d6cd055760f797eb1d6
Haris <harisgujjar317@gmail.com>
2024-04-29 16:27:20 -0400

    fixing mobile issues

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -11,29 +11,29 @@ export default function Home() {
     
     useEffect(() => { 
         gsap.from('.home', { y: -60, opacity: 0 });
-        gsap.to('.home', 1, { y:100, ease: 'easeOut', opacity: 1, });   
+        gsap.to('.home', 1, { y:0, ease: 'easeOut', opacity: 1, });   
       })
     return (
         <div className='home'>
-            <div className='px-10 md:px-0'>
-                <div className='text-center mx-auto relative z-50'>
-                    <h1 className='font-bold text-3xl md:text-6xl font-montserrat'>Front-End React Developer <img className='inline w-10 h-10' src={wavingHand} alt='wavingHand' /> </h1>
-                    <p className='px-7 mt-6 font-medium text-base md:text-lg font-montserrat text-stone-400'>Hi, I'm Haris Khan. A passionate Frontend Developer based in New York, United States <img className='inline w-6 h-6' src={pinSeo} alt='pinSeo' /></p>
-                    <div className='flex justify-center gap-5 mt-6'>
-                        <a href="https://github.com/hariskhan317" target="_blank" rel="noreferrer">
-                            <AiFillGithub size={40} />
-                        </a>
-                        <a href="https://www.linkedin.com/in/haris-khan-9b6449292/" target="_blank" rel="noreferrer">
-                            <FaLinkedin size={40} />
-                        </a>
+                <div className='text-center mx-auto absolute top-[30%] md:top-[40%] bottom-0 left-0 right-0 z-50'>
+                    <div className='px-10 md:px-0'>
+                        <h1 className='font-bold text-3xl md:text-6xl font-montserrat'>Front-End React Developer <img className='inline w-10 h-10' src={wavingHand} alt='wavingHand' /> </h1>
+                        <p className='px-7 mt-6 font-medium text-base md:text-lg font-montserrat text-stone-400'>Hi, I'm Haris Khan. A passionate Frontend Developer based in New York, United States <img className='inline w-6 h-6' src={pinSeo} alt='pinSeo' /></p>
+                        <div className='flex justify-center gap-5 mt-6'>
+                            <a href="https://github.com/hariskhan317" target="_blank" rel="noreferrer">
+                                <AiFillGithub size={40} />
+                            </a>
+                            <a href="https://www.linkedin.com/in/haris-khan-9b6449292/" target="_blank" rel="noreferrer">
+                                <FaLinkedin size={40} />
+                            </a>
+                        </div>
                     </div>
                 </div>
                 <div className=''> 
-                    <div className='absolute top-[-500px] mx-auto -right-96 z-0'>
+                    <div className=' mx-auto z-0'>
                         <Ball/>
                     </div>
                 </div>
-            </div>
             <TechStack />
         </div>
     )
--- src/components/Home/techStack.jsx
+++ src/components/Home/techStack.jsx
@@ -5,17 +5,17 @@ import gsap from 'gsap';
 export default function TechStack() {
     useEffect(() => { 
         gsap.from('.techstack', { y: 260, opacity: 0 });
-        gsap.to('.techstack', 2.3, { y: 0, ease: 'Bounce.easeOut', opacity: 1, });  
+        gsap.to('.techstack', 2.3, { y: -310, ease: 'Bounce.easeOut', opacity: 1, });  
     })
 
     return (
-        <div className='techstack flex flex-col lg:flex-row justify-center gap-10 mt-10 md:mt-28 px-10 md:px-0 z-30 relative'>
+        <div className='techstack flex flex-col lg:flex-row justify-center gap-10 mt-10 md:mt-0 px-10 md:px-0 z-30 relative'>
             <div className='border-r-0 md:border-r-2 py-2 pr-5 border-stone-300'>
                 <p className='font-semibold text-base font-montserrat text-center md:text-left'>Tech Stack</p>
             </div>
             <div className='flex gap-8 flex-wrap justify-center'>
                 {Skills.map((skill) => (
-                    <img src={skill} className='w-9 h-9' alt="skill" />
+                    <img src={skill} className='w-8 h-8 md:w-9 md:h-9' alt="skill" />
                 ))}
             </div>
         </div>
