395d4ba0ed1e1d53ec921e9924983cc107839a21
Haris <harisgujjar317@gmail.com>
2024-05-24 12:34:45 -0400

    added more projects x

---
+++ src/components/Projects/projectList.js
@@ -0,0 +1,52 @@
+import RatePin from '../../assets/ratepin.png'
+import Spendex from '../../assets/spendex.png'
+import WanderWise from '../../assets/wanderwise.png'
+import reactSvg from '../../assets/react.svg'
+import tailwind from '../../assets/tailwindcss.svg'
+import javaScript from '../../assets/javascript.svg'
+import ChatgptClone from '../../assets/ChatgptClone.png'
+import express from '../../assets/express.svg'
+import mongodb from '../../assets/mongodb.svg'
+import node from '../../assets/node2.svg' 
+import { MdLocationPin } from "react-icons/md";
+import { LuBrainCircuit } from "react-icons/lu";
+import { GrCalculator } from "react-icons/gr";
+
+export const projects = [
+    {
+        'img': Spendex,
+        'icon': <GrCalculator className='mt-1 mr-2 text-sky-500' />,
+        'name': 'SpendeX',
+        'techstack': [reactSvg, tailwind, javaScript, express, mongodb, node],
+        'description': "I developed a MERN stack application for tracking monthly budgets, enabling users to add and manage their budget and expenses. It uses JWT for secure authentication, React Router for navigation, and React's Context API for state management. The backend is built with Express and Node.js, with MongoDB for data storage.",
+        'githubLink': 'https://github.com/hariskhan317/Finance-Tracker',
+        'liveDemo': 'https://finance-tracker-plum.vercel.app/'
+    },
+    {
+        'img': ChatgptClone,
+        'icon': <LuBrainCircuit className='mt-1 mr-1 text-sky-500' />,
+        'name': 'Chatgpt Clone',
+        'techstack': [reactSvg, tailwind, javaScript, express, mongodb, node],
+        'description': "I created a modern clone of ChatGPT using the MERN stack, integrating JWT for authentication and an auth context for seamless data management. This application replicates ChatGPT's functionality with a user-friendly interface, enabling efficient communication and interaction.",
+        'githubLink': 'https://github.com/hariskhan317/AiChatApp',
+        'liveDemo': ''
+    },
+    {
+        'img': WanderWise,
+        'icon': <MdLocationPin className='mt-1 text-sky-500' />,
+        'name': 'Wander Wise',
+        'techstack': [reactSvg, tailwind, javaScript],
+        'description': 'Discover the Wander Wise app, your ultimate guide to restaurants and reviews. With this application, explore reviews, find addresses, and visualize locations on the map. Developed using React and Tailwind CSS, it integrates Rapid API and Google Maps API to seamlessly fetch data.',
+        'githubLink': 'https://github.com/hariskhan317/wanderwiseProject',
+        'liveDemo': 'https://wander-wise.netlify.app/'
+    },
+    {
+        'img': RatePin,
+        'icon': <MdLocationPin className='mt-1 text-sky-500' />,
+        'name': 'RatePin',
+        'techstack': [reactSvg, tailwind, javaScript, express, mongodb, node],
+        'description': 'Discover, rate, and review places effortlessly with our intuitive map interface. Share your experiences, leave detailed reviews, and provide feedback with our One-Click Rating feature. Plus, say goodbye to manual input – our app automatically captures location details for you!',
+        'githubLink': 'https://github.com/hariskhan317/RatePin',
+        'liveDemo': 'https://rate-pin-frontend.vercel.app/'
+    }
+]
--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -1,15 +1,9 @@
 import React, {useEffect} from 'react'
 import { MdLocationPin } from "react-icons/md";
 import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
-import RatePin from '../../assets/ratepin.png'
-import WanderWise from '../../assets/wanderwise.png'
-import reactSvg from '../../assets/react.svg'
-import tailwind from '../../assets/tailwindcss.svg'
-import javaScript from '../../assets/javascript.svg'
-import express from '../../assets/express.svg'
-import mongodb from '../../assets/mongodb.svg'
-import node from '../../assets/node2.svg' 
+
 import gsap from 'gsap'; 
+import { projects } from './projectList';
 
 
 
@@ -34,85 +28,45 @@ export default function Projects() {
                 <p className='text-center mt-1 font-bold text-lg md:text-2xl font-montserrat text-white'>Each Project is a Unique piece of Development 💻 </p>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-20'>
-                {/* project 1 */}
-                <div className='projects-left grid grid-cols-1 md:grid-cols-5 gap-6 md:mt-14'>
-                    <div className='hidden md:block col-span-1 md:col-span-2'>
-                        <a href='https://wander-wise.netlify.app/' target="_blank" rel="noreferrer" >
-                            <img src={WanderWise} className='mt-4 w-full h-full md:h-44 rounded-lg' alt="" />
-                        </a>
-                    </div>
-                    <div className='col-span-1 md:col-span-3 mt-10 md:mt-4'>
-                        <div className='flex justify-between'>
-                            <div className='flex text-xl md:text-2xl'>
-                                <MdLocationPin className='mt-1 text-sky-500' />
-                                <p className='text-center font-bold text-sky-500 font-montserrat'>Wander Wise</p>
-                            </div>
-                            <div className='techstack flex gap-5 mt-1'>
-                                <img src={reactSvg} className='w-6 h-6' alt="reactSvg" />
-                                <img src={tailwind} className='w-6 h-6' alt="tailwind" />
-                                <img src={javaScript} className='w-6 h-6' alt="javaScript" /> 
-                            </div>  
+                {projects.map((project, index) => (
+                    <div key={index} className='projects-left grid grid-cols-1 md:grid-cols-5 gap-6 md:mt-14'>
+                        <div className='hidden md:block col-span-1 md:col-span-2'>
+                            <a href={project.liveDemo} target="_blank" rel="noreferrer" >
+                                <img src={project.img} className='mt-4 w-full h-full md:h-44 rounded-lg' alt="" />
+                            </a>
                         </div>
-                        <div className='pt-2 pb-3'>
-                            <p className='font-medium text-base py-4 md:py-0 md:text-[13px] font-montserrat text-stone-400'> Discover the Wander Wise app, your ultimate guide to restaurants and reviews. With this application, explore reviews, find addresses, and visualize locations on the map. Developed using React and Tailwind CSS, it integrates Rapid API and Google Maps API to seamlessly fetch data.</p>
-                        </div>
-                        <div className='flex justify-start gap-12 pl-3'>
-                            <div>
-                                <a href='https://github.com/hariskhan317/wanderwiseProject' target="_blank" rel="noreferrer" className='flex gap-1.5 text-base text-stone-400 underline font-semibold font-montserrat'>
-                                    Code
-                                    <FaGithub className='mt-0.5' />
-                                </a>
+                        <div className='col-span-1 md:col-span-3 mt-10 md:mt-4'>
+                            <div className='flex justify-between'>
+                                <div className='flex text-xl md:text-2xl'>
+                                    <div>{ project.icon}</div>
+                                    <p className='text-center font-bold text-lg mt-0.5 text-sky-500 font-montserrat'>{project.name}</p>
+                                </div>
+                                <div className='techstack flex gap-3 mt-1'> 
+                                    {project.techstack.map((item) => (
+                                        <img src={item} className='w-5 h-5' alt="reactSvg" />
+                                    ))}
+                                </div>  
                             </div>
-                            <div>
-                                <a href='https://wander-wise.netlify.app/' target="_blank" rel="noreferrer" className='flex gap-1.5 text-base text-stone-400 underline font-semibold font-montserrat'>
-                                    Live Demo
-                                    <FaExternalLinkAlt className='mt-0.5' />
-                                </a>
+                            <div className='pt-2 pb-3'>
+                                <p className='font-medium text-base py-4 md:py-0 md:text-[13px] font-montserrat text-stone-400'> {project.description}</p>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                {/* project 2 */}
-                <div className='projects-right grid grid-cols-1 md:grid-cols-5 gap-6 md:mt-14'>
-                    <div className='hidden md:block col-span-1 md:col-span-2'>
-                        <a href='https://rate-pin-frontend.vercel.app/' target="_blank" rel="noreferrer" >
-                            <img src={RatePin} className='mt-4 w-full h-full md:h-44 rounded-lg' alt="" />
-                        </a>
-                    </div>
-                    <div className='col-span-1 md:col-span-3 mt-10 md:mt-4'>
-                        <div className='flex justify-between'>
-                            <div className='flex text-xl md:text-2xl'>
-                                <MdLocationPin className='mt-1 text-sky-500' />
-                                <p className='text-center font-bold text-sky-500 font-montserrat'>RatePin</p>
-                            </div>
-                            <div className='techstack flex gap-5 mt-1'>
-                                <img src={reactSvg} className='w-6 h-6' alt="reactSvg" />
-                                <img src={tailwind} className='w-6 h-6' alt="tailwind" />
-                                <img src={javaScript} className='w-6 h-6' alt="javaScript" />
-                                <img src={express} className='w-6 h-6' alt="express" />
-                                <img src={mongodb} className='w-6 h-6' alt="mongodb" />
-                                <img src={node} className='w-6 h-6' alt="node" />
-                            </div>  
-                        </div>
-                        <div className='pt-2 pb-3'>
-                            <p className='font-medium py-4 md:py-0 md:text-[13px] font-montserrat text-stone-400'>Discover, rate, and review places effortlessly with our intuitive map interface. Share your experiences, leave detailed reviews, and provide feedback with our One-Click Rating feature. Plus, say goodbye to manual input – our app automatically captures location details for you!</p>
-                        </div>
-                        <div className='flex justify-start gap-12 pl-3'>
-                            <div>
-                                <a href='https://github.com/hariskhan317/RatePin' target="_blank" rel="noreferrer" className='flex gap-1.5 text-base font-semibold text-stone-400 underline font-montserrat'>
-                                    Code
-                                    <FaGithub className='mt-0.5' />
-                                </a>
-                            </div>
-                            <div>
-                                <a href='https://rate-pin-frontend.vercel.app/' target="_blank" rel="noreferrer" className='flex gap-1.5 text-base font-semibold text-stone-400 underline font-montserrat'>
-                                    Live Demo
-                                    <FaExternalLinkAlt className='mt-0.5' />
-                                </a>
+                            <div className='flex justify-start gap-12 pl-3'>
+                                <div>
+                                    <a href={project.githubLink} target="_blank" rel="noreferrer" className='flex gap-1.5 text-base text-stone-400 underline font-semibold font-montserrat'>
+                                        Code
+                                        <FaGithub className='mt-0.5' />
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href={project.liveDemo} target="_blank" rel="noreferrer" className='flex gap-1.5 text-base text-stone-400 underline font-semibold font-montserrat'>
+                                        Live Demo
+                                        <FaExternalLinkAlt className='mt-0.5' />
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                ))}
             </div>
         </div>
     )
